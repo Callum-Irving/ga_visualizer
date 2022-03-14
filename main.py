b85832f74adcb333df55d51c1636756d0e5e8806
Callum Irving <callum.irving@protonmail.com>
2022-03-13 23:34:48 -0400

    add population class

--- main.py
+++ main.py
@@ -11,10 +11,25 @@ class Genome:
         self.y = randint(0, ymax)
 
 
-def get_err(guess: Genome, target: tuple[int, int]) -> float:
-    """Return squared distance between vectors."""
+def fitness(guess: Genome, target: tuple[int, int]) -> float:
+    """Return 1 / squared distance between vectors."""
     xdist = guess.x - target[0]
     ydist = guess.y - target[0]
 
     # a^2 + b^2 = c^2
-    return xdist**2 + ydist**2
+    return 1 / (xdist**2 + ydist**2)
+
+
+class Population:
+    points: list[Genome]
+    generation: int
+
+    def __init__(self, size: int, x: int, y: int):
+        """Create new population with `size` members."""
+        self.points = []
+        for _ in range(size):
+            self.points.append(Genome(x, y))
+
+    def do_generation(self):
+        # TODO
+        pass
